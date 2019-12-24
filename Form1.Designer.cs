2bcc1feb8ea00f6832fc9d35b5593b9f15e7de3a
Solomatin Bogdan <hurricaneruinner@gmail.com>
2019-12-24 18:50:38 +0600

    [+] Added Search by location

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,203 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.printPreviewDialog1 = new System.Windows.Forms.PrintPreviewDialog();
+            this.lbl_cityname = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lbl_units = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.lbl_temp = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbl_wind = new System.Windows.Forms.Label();
+            this.lbl_humidity = new System.Windows.Forms.Label();
+            this.lbl_lat = new System.Windows.Forms.Label();
+            this.lbl_lon = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // printPreviewDialog1
+            // 
+            this.printPreviewDialog1.AutoScrollMargin = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.AutoScrollMinSize = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.ClientSize = new System.Drawing.Size(400, 300);
+            this.printPreviewDialog1.Enabled = true;
+            this.printPreviewDialog1.Icon = ((System.Drawing.Icon)(resources.GetObject("printPreviewDialog1.Icon")));
+            this.printPreviewDialog1.Name = "printPreviewDialog1";
+            this.printPreviewDialog1.Visible = false;
+            // 
+            // lbl_cityname
+            // 
+            this.lbl_cityname.AutoSize = true;
+            this.lbl_cityname.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lbl_cityname.Location = new System.Drawing.Point(19, 27);
+            this.lbl_cityname.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbl_cityname.Name = "lbl_cityname";
+            this.lbl_cityname.Size = new System.Drawing.Size(93, 46);
+            this.lbl_cityname.TabIndex = 0;
+            this.lbl_cityname.Text = "filler";
+            this.lbl_cityname.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(511, 270);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(78, 71);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.PictureBox1_Click);
+            // 
+            // lbl_units
+            // 
+            this.lbl_units.AutoSize = true;
+            this.lbl_units.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lbl_units.Location = new System.Drawing.Point(119, 84);
+            this.lbl_units.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbl_units.Name = "lbl_units";
+            this.lbl_units.Size = new System.Drawing.Size(49, 46);
+            this.lbl_units.TabIndex = 2;
+            this.lbl_units.Text = "C";
+            this.lbl_units.Click += new System.EventHandler(this.Label2_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(27, 146);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(69, 17);
+            this.radioButton1.TabIndex = 4;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Цельсий";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.RadioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(27, 169);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(82, 17);
+            this.radioButton2.TabIndex = 5;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Фаренгейт";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.RadioButton2_CheckedChanged);
+            // 
+            // lbl_temp
+            // 
+            this.lbl_temp.AutoSize = true;
+            this.lbl_temp.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lbl_temp.Location = new System.Drawing.Point(19, 84);
+            this.lbl_temp.Name = "lbl_temp";
+            this.lbl_temp.Size = new System.Drawing.Size(35, 46);
+            this.lbl_temp.TabIndex = 6;
+            this.lbl_temp.Text = "*";
+            this.lbl_temp.Click += new System.EventHandler(this.Lbl_temp_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(260, 110);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Ветер:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(260, 84);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Влажность:";
+            this.label1.Click += new System.EventHandler(this.Label1_Click_2);
+            // 
+            // lbl_wind
+            // 
+            this.lbl_wind.AutoSize = true;
+            this.lbl_wind.Location = new System.Drawing.Point(306, 110);
+            this.lbl_wind.Name = "lbl_wind";
+            this.lbl_wind.Size = new System.Drawing.Size(63, 13);
+            this.lbl_wind.TabIndex = 9;
+            this.lbl_wind.Text = "Влажность";
+            this.lbl_wind.Click += new System.EventHandler(this.Label3_Click);
+            // 
+            // lbl_humidity
+            // 
+            this.lbl_humidity.AutoSize = true;
+            this.lbl_humidity.Location = new System.Drawing.Point(332, 84);
+            this.lbl_humidity.Name = "lbl_humidity";
+            this.lbl_humidity.Size = new System.Drawing.Size(63, 13);
+            this.lbl_humidity.TabIndex = 10;
+            this.lbl_humidity.Text = "Влажность";
+            // 
+            // lbl_lat
+            // 
+            this.lbl_lat.AutoSize = true;
+            this.lbl_lat.Location = new System.Drawing.Point(260, 150);
+            this.lbl_lat.Name = "lbl_lat";
+            this.lbl_lat.Size = new System.Drawing.Size(46, 13);
+            this.lbl_lat.TabIndex = 11;
+            this.lbl_lat.Text = "Щирота";
+            this.lbl_lat.Click += new System.EventHandler(this.Lbl_lan_Click);
+            // 
+            // lbl_lon
+            // 
+            this.lbl_lon.AutoSize = true;
+            this.lbl_lon.Location = new System.Drawing.Point(260, 173);
+            this.lbl_lon.Name = "lbl_lon";
+            this.lbl_lon.Size = new System.Drawing.Size(50, 13);
+            this.lbl_lon.TabIndex = 12;
+            this.lbl_lon.Text = "Долгота";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.lbl_lon);
+            this.Controls.Add(this.lbl_lat);
+            this.Controls.Add(this.lbl_humidity);
+            this.Controls.Add(this.lbl_wind);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbl_temp);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.lbl_units);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lbl_cityname);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "c";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PrintPreviewDialog printPreviewDialog1;
+        private System.Windows.Forms.Label lbl_cityname;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lbl_units;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Label lbl_temp;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbl_wind;
+        private System.Windows.Forms.Label lbl_humidity;
+        private System.Windows.Forms.Label lbl_lat;
+        private System.Windows.Forms.Label lbl_lon;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -2,19 +2,139 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Device.Location;
 using System.Drawing;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Newtonsoft.Json;
 
 namespace Weather
 {
     public partial class Form1 : Form
     {
+        const string APIID = "846f12aa31d2907a0bbb26f484c1c60f";
+        string units = "metric";
+        private GeoCoordinateWatcher Watcher = null;
         public Form1()
         {
             InitializeComponent();
+        }
+
+
+
+        void getWeather(double lat,double lon) {
+            using (WebClient web = new WebClient())
+            {
+                string url = string.Format("https://api.openweathermap.org/data/2.5/weather?lat={0}&lon={1}&units={2}&appid={3}",lat,lon, units, APIID);
+                var json = web.DownloadString(url);
+                var result = JsonConvert.DeserializeObject<WeatherInfo.root>(json);
+                WeatherInfo.root output = result;
+                lbl_cityname.Text = string.Format("{0} {1}", output.name, output.sys.country);
+                lbl_units.Text = "\u00B0C";
+                lbl_temp.Text = string.Format("{0}", output.main.temp);
+                lbl_humidity.Text = string.Format("{0}%", output.main.humidity);
+                lbl_wind.Text = string.Format("{0} м/с", output.wind.speed);
+            }
+        }
+        void getWeather(string cityName)
+        {
+            using (WebClient web = new WebClient())
+            {
+                string url = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=" + "metric" + "&appid=" + APIID;
+                var json = web.DownloadString(url);
+                var result = JsonConvert.DeserializeObject<WeatherInfo.root>(json);
+                WeatherInfo.root output = result;
+                lbl_cityname.Text = string.Format("{0} {1}", output.name, output.sys.country);
+                lbl_units.Text = "\u00B0C";
+                lbl_temp.Text = string.Format("{0}", output.main.temp);
+                lbl_humidity.Text = string.Format("{0}%", output.main.humidity);
+                lbl_wind.Text = string.Format("{0} м/с", output.wind.speed);
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Watcher = new GeoCoordinateWatcher();
+
+            Watcher.StatusChanged += Watcher_StatusChanged;
+
+            Watcher.Start();
+        }
+
+        private void Watcher_StatusChanged(object sender,
+    GeoPositionStatusChangedEventArgs e)
+        {
+            if (e.Status == GeoPositionStatus.Ready)
+            {
+                // Display the latitude and longitude.
+                if (Watcher.Position.Location.IsUnknown)
+                {
+                    lbl_cityname.Text = "Cannot find location data";
+                }
+                else
+                {
+                    GeoCoordinate location =
+                    Watcher.Position.Location;
+                    double lat, lon;
+                    lat = location.Latitude;
+                    lon = location.Longitude;
+                    lbl_lat.Text = location.Latitude.ToString();
+                    lbl_lon.Text = location.Longitude.ToString();
+                    getWeather(lat,lon);
+                }
+            }
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PictureBox1_Click(object sender, EventArgs e)
+        {
+            getWeather("Moscow");
+        }
+
+        private void Label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RadioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Lbl_temp_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label1_Click_2(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RadioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Lbl_lan_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
